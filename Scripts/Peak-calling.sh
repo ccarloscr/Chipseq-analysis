d90db098c9113b7692529773f07e2156e96a0cda
ccarloscr <134629785+ccarloscr@users.noreply.github.com>
2025-02-11 13:27:39 +0100

    Update Peak-calling.sh

--- Scripts/Peak-calling.sh
+++ Scripts/Peak-calling.sh
@@ -5,22 +5,40 @@
 ## For strand-specific reads, remove the --nomodel and --extsize options of the macs2 callpeak
 
 # Variable set up
-INPUT_DIR="Histone_ChIPseq/Mapped/Sorted"
-OUTPUT_DIR="Histone_ChIPseq/Peaks"
+INPUT_DIR="Chipseq-analysis/Mapped/Sorted"
+OUTPUT_DIR="Chipseq-analysis/Peaks"
+METADATA_FILE="Chipseq-analysis/metadata.tsv"
 INPUT_NAME=""      # Name of input file (no antibody)
 EXP_NAME=""        # Name of experimental file (antibody)
 EXTSIZE="150"      # Average fragment length (i.e. minimal peak size)
 
 
+mkdir -p "$OUTPUT_DIR"
 
-mkdir -p $OUTPUT_DIR
 
-# Peak calling using MACS2
-for bam_file in $INPUT_DIR/*.bam; do
-    # Create variable name
-    base_name=$(basename "$fastq_file" _sorted.bam)
-    # Define output name
-    output_sam="$OUTPUT_DIR/${base_name}_mapped.sam"
-    # Peak calling
-    macs2 callpeak -t bam_file -c ??? -f BAM -g dm --outdir $OUTPUT_DIR -n ?? --nomodel --extsize $EXTSIZE
+## Read the first 4 columns of the metadata file
+while IFS=$'\t' read -r INPUT_NAME EXP_NAME ANTIBODY CONDITION REP _; do
+    INPUT_BAM="$INPUT_DIR/${INPUT_NAME}_sorted.bam"
+    EXP_BAM="$INPUT_DIR/${EXP_NAME}_sorted.bam"
+    OUTPUT_NAME="${ANTIBODY}_${CONDITION}_${REP}"
+
+    # Verify if the input file exists
+    if [[ ! -f "$INPUT_BAM" ]]; then
+        echo "Error: Input $INPUT_BAM not found"
+        continue
+    fi
+
+    # Verify if the experimental chipseq file exists
+    if [[ ! -f "$EXP_BAM" ]]; then
+        echo "Error: Experimental Chip-seq $EXP_BAM not found"
+        continue
+    fi
+
+    macs2 callpeak -t "$EXP_BAM" -c "$INPUT_BAM" -f BAM -g dm --outdir "$OUTPUT_DIR" -n "$OUTPUT_NAME" --nomodel --extsize "$EXTSIZE"
+
+    echo "Peak calling done for: $EXP_NAME vs $INPUT_NAME"
+
+done < <(tail -n +2 "$METADATA_FILE")
+
+echo "Peak calling complete for ${METADATA_FILE}."
 
