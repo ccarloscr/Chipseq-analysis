ccc88d6ce38ae2b7b3a5b068493017f2672debba
ccarloscr <134629785+ccarloscr@users.noreply.github.com>
2025-02-11 13:31:31 +0100

    Update Peak-calling.sh

--- Scripts/Peak-calling.sh
+++ Scripts/Peak-calling.sh
@@ -16,7 +16,7 @@ EXTSIZE="150"      # Average fragment length (i.e. minimal peak size)
 mkdir -p "$OUTPUT_DIR"
 
 
-## Read the first 4 columns of the metadata file
+## Read the first 5 columns of the metadata file
 while IFS=$'\t' read -r INPUT_NAME EXP_NAME ANTIBODY CONDITION REP _; do
     INPUT_BAM="$INPUT_DIR/${INPUT_NAME}_sorted.bam"
     EXP_BAM="$INPUT_DIR/${EXP_NAME}_sorted.bam"
@@ -24,18 +24,18 @@ while IFS=$'\t' read -r INPUT_NAME EXP_NAME ANTIBODY CONDITION REP _; do
 
     # Verify if the input file exists
     if [[ ! -f "$INPUT_BAM" ]]; then
-        echo "Error: Input $INPUT_BAM not found"
+        echo "Error: Input $INPUT_BAM not found" >&2
         continue
     fi
 
     # Verify if the experimental chipseq file exists
     if [[ ! -f "$EXP_BAM" ]]; then
-        echo "Error: Experimental Chip-seq $EXP_BAM not found"
+        echo "Error: Experimental Chip-seq $EXP_BAM not found" >&2
         continue
     fi
 
+    # Peak calling using MACS2
     macs2 callpeak -t "$EXP_BAM" -c "$INPUT_BAM" -f BAM -g dm --outdir "$OUTPUT_DIR" -n "$OUTPUT_NAME" --nomodel --extsize "$EXTSIZE"
-
     echo "Peak calling done for: $EXP_NAME vs $INPUT_NAME"
 
 done < <(tail -n +2 "$METADATA_FILE")
