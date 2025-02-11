adef3590a66c23cdd0cc857864fb25b08a824c42
ccarloscr <134629785+ccarloscr@users.noreply.github.com>
2025-02-11 13:43:47 +0100

    Update Peak-filtering.sh

--- Scripts/Peak-filtering.sh
+++ Scripts/Peak-filtering.sh
@@ -1,16 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env Rscript
 
 ## Comments
-## This peak calling is optimized for reads with no strand information
-## For strand-specific reads, remove the --nomodel and --extsize options of the macs2 callpeak
-
-# Variable set up
-INPUT_DIR="Histone_ChIPseq/Mapped/Sorted"
-OUTPUT_DIR="Histone_ChIPseq/Peaks"
-INPUT_NAME=""      # Name of input file (no antibody)
-EXP_NAME=""        # Name of experimental file (antibody)
-EXTSIZE="150"      # Average fragment length (i.e. minimal peak size)
+## This script removes peaks located in non-canonical chromosomes, which are abundant in Drosophila melanogaster
 
 
 
-mkdir -p $OUTPUT_DIR
+
