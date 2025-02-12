b3a3c60602b712b82f3ab73c8896e89ae3dd67c8
ccarloscr <134629785+ccarloscr@users.noreply.github.com>
2025-02-12 18:16:07 +0100

    Update Post-map-process.sh

--- Scripts/Post-map-process.sh
+++ Scripts/Post-map-process.sh
@@ -2,57 +2,53 @@
 
 # Author: Carlos Camilleri-Robles
 # Contact: carloscamilleri@hotmail.com
+# Version:
 # This script uses samtools to filter, sort and index the resulting .bam files of an alignment
 
 set -e
 
 ## Variable set up
-INPUT_DIR="Chipseq-analysis/Mapped"
-FILTERED_DIR="Chipseq-analysis/Mapped/Filtered"
-SORTED_DIR="Chipseq-analysis/Mapped/Sorted"
-MAX_MISMATCH="4"  # Only works for up to 9 mismatches
+INPUT_BAM="$1"
+FILTERED_DIR="$2"
+SORTED_DIR="$3"
+MAX_MISMATCH="$4"
 
 
-## Create output folder
-mkdir -p $FILTERED_DIR
-mkdir -p $SORTED_DIR
+## Create directories
+mkdir -p "$FILTERED_DIR" "$SORTED_DIR"
 
 
-## Processing of mapped .bam files
+## Get basename of input .bam files
+base_name=$(basename "$INPUT_BAM" .bam)
+base_name=${base_name%_mapped}
 
-for bam_file in $INPUT_DIR/*.bam; do
+## Define outputs
+filtered_bam="$FILTERED_DIR/${base_name}_filtered.bam"
+sorted_bam="$SORTED_DIR/${base_name}_sorted.bam"
 
-    # Create variable based on mapped bam filename
-    base_name=$(basename "$bam_file" _mapped.bam)
+
+## Filter reads with less mismatches than max set
+echo "Filtering $INPUT_BAM..."
+if ! samtools view -h "$INPUT_BAM" | grep -E "^@|NM:i:[0-$MAX_MISMATCH]" | samtools view -bS - > "$filtered_bam"; then
+    echo "Error filtering $INPUT_BAM" >&2
+    exit 1
+fi
+
     
-    # Define output names
-    filtered_bam="$FILTERED_DIR/${base_name}_filtered.bam"
-    sorted_bam="$SORTED_DIR/${base_name}_sorted.bam"
-    
-    # Filter out reads containing less than $MAX_MISMATCH mismatches
-    echo "Filtering $bam_file..."
-    if !  samtools view -h "$bam_file" | grep -E "^@|NM:i:[0-$MAX_MISMATCH]" | samtools view -bS - > "$filtered_bam"; then          
-          echo "Error filtering $bam_file" >&2
-    else
-          echo "Filtering of $bam_file complete."
-    fi
-    
-    # Sorting of bam files
-    echo "Sorting $filtered_bam..."
-    if !  samtools sort -o "$sorted_bam" "$filtered_bam"; then
-          echo "Error sorting $bam_file" >&2
-    else
-          echo "Sorting of $bam_file complete."
-    fi
+## Sort filtered bam files
+echo "Sorting $filtered_bam..."
+if ! samtools sort -o "$sorted_bam" "$filtered_bam"; then
+    echo "Error sorting $filtered_bam" >&2
+    exit 1
+fi
 
-    # Indexing of bam files
-    echo "Indexing $filtered_bam..."
-    if !  samtools index "$sorted_bam"; then
-          echo "Error indexing $bam_file" >&2
-    else
-          echo "Indexing of $bam_file complete."
-    fi
 
-    echo "Processing of $bam_file complete"
-done
+## Indexing sorted bam files
+echo "Indexing $sorted_bam..."
+if ! samtools index "$sorted_bam"; then
+    echo "Error indexing $sorted_bam" >&2
+    exit 1
+fi
 
+
+echo "Processing of $INPUT_BAM done."
