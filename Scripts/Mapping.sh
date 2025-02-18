f47a0c1a7cb657a1554ccaac59c0a1511173d82e
ccarloscr <134629785+ccarloscr@users.noreply.github.com>
2025-02-18 13:19:35 +0100

    Update Mapping.sh

--- Scripts/Mapping.sh
+++ Scripts/Mapping.sh
@@ -2,7 +2,7 @@
 
 # Author: Carlos Camilleri-Robles
 # Contact: carloscamilleri@hotmail.com
-# Version: updated 12-02-2025
+# Version: updated 18-02-2025
 # This script uses HISAT2 to map single-end reads to dm3
 
 set -e
@@ -10,7 +10,7 @@ set -e
 
 ## Variable set up
 REFERENCE_GENOME=$1
-FASTQ_DIR=$2
+FASTQ_FILE=$2
 OUTPUT_DIR=$3
 
 
@@ -26,35 +26,29 @@ fi
 
 
 ## Check if the input directory exists
-if [[ ! -d "$FASTQ_DIR" ]]; then
-    echo "Error: The input directory "$FASTQ_DIR" does not exist." >&2
+if [[ ! -f "$FASTQ_FILE" ]]; then
+    echo "Error: The input file "$FASTQ_DIR" does not exist." >&2
     exit 1
 fi
 
 
-## Mapping using HISAT2
-for fastq_file in "$FASTQ_DIR"/*.fastq; do
-
-    # Create variable based on fastq filename
-    base_name=$(basename "$fastq_file" .fastq)
+# Create variable based on fastq filename
+base_name=$(basename "$FASTQ_FILE" .fastq)
     
-    # Define output
-    output_sam="$OUTPUT_DIR/${base_name}_mapped.sam"
-    output_bam="$OUTPUT_DIR/${base_name}_mapped.bam"
+# Define output
+output_sam="$OUTPUT_DIR/${base_name}_mapped.sam"
+output_bam="$OUTPUT_DIR/${base_name}_mapped.bam"
     
-    # Mapping of single-end fastq files
-    hisat2 -x "$REFERENCE_GENOME" -U "$fastq_file" -S "$output_sam"
+# Mapping of single-end fastq files
+hisat2 -x "$REFERENCE_GENOME" -U "$FASTQ_FILE" -S "$output_sam"
     
-    # Conversion of sam files into bam files
-    samtools view -bS "$output_sam" > "$output_bam"
+# Conversion of sam files into bam files
+samtools view -bS "$output_sam" > "$output_bam"
     
-    # Check if bam file is empty
-    if [[ ! -s "$output_bam"]]; then
-        echo "Error: BAM file is empty for "$fastq_file"." >&2
-        continue  
-    fi
+# Check if bam file is empty
+if [[ ! -s "$output_bam"]]; then
+    echo "Error: BAM file is empty for "$FASTQ_FILE"." >&2
+    continue  
+fi
     
-    echo "Mapping for "$fastq_file" completed." >&2
-done
-
-echo "All mappings completed."
+echo "Mapping for "$FASTQ_FILE" completed." >&2
