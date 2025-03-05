30f9941f151598d5b8733add6c35bb587be7d1f7
ccarloscr <134629785+ccarloscr@users.noreply.github.com>
2025-03-05 12:43:04 +0100

    Update Mapping.sh

--- Scripts/Mapping.sh
+++ Scripts/Mapping.sh
@@ -2,28 +2,32 @@
 
 # Author: Carlos Camilleri-Robles
 # Contact: carloscamilleri@hotmail.com
-# Version: updated 24-02-2025
+# Version: updated 05-03-2025
 # This script uses HISAT2 to map single-end reads to dm3
 
 set -e
 
+## Check that conda encironment is activated
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate workplace || { echo "Error: Conda environment not activated!"; exit 1; }
+echo "Using HISAT2 from: $(which hisat2)"
 
 ## Variable set up
-REFERENCE_GENOME=$1
-FASTQ_FILE=$2
+FASTQ_FILE=$1
+REFERENCE_GENOME=$2
 OUTPUT_DIR=$3
 
-
-## Check if the reference genome is available
-if [[ ! -f "${REFERENCE_GENOME}/dm3_index.1.ht2" ]]; then
-    echo "Error: reference genome index files are not available." >&2
+## Check if the input fastq file is correct
+echo "Checking file: $FASTQ_FILE"
+if [[ ! -f $FASTQ_FILE ]]; then
+    echo "Error: The input file $FASTQ_FILE does not exist." >&2
     exit 1
 fi
 
-
-## Check if the input directory exists
-if [[ ! -f "$FASTQ_FILE" ]]; then
-    echo "Error: The input file $FASTQ_FILE does not exist." >&2
+## Check if the reference genome is available
+echo "Checking refrence genome index: $REFERENCE_GENOME"
+if [[ ! -f "${REFERENCE_GENOME}/dm3_index.1.ht2" ]]; then
+    echo "Error: reference genome index files are not available." >&2
     exit 1
 fi
 
