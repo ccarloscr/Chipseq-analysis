4efa931907cdbb34969893e4a039134a6f96b49f
ccarloscr <134629785+ccarloscr@users.noreply.github.com>
2025-03-05 17:41:01 +0100

    Update run_chipseq.sh

--- run_chipseq.sh
+++ run_chipseq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=chipseq
 #SBATCH --partition=irbio01
-#SBATCH --cpus-per-task=12
+#SBATCH --cpus-per-task=24
 #SBATCH --time=10:00:00
 #SBATCH --output=chipseq_%j.out
 #SBATCH --error=chipseq_%j.err
