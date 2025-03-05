d32295f44ef1479f6e9a93ab04ba2cd5ede31e4d
ccarloscr <134629785+ccarloscr@users.noreply.github.com>
2025-03-05 13:19:13 +0100

    Update Post-map-process.sh

--- Scripts/Post-map-process.sh
+++ Scripts/Post-map-process.sh
@@ -13,11 +13,6 @@ FILTERED_DIR="$2"
 SORTED_DIR="$3"
 MAX_MISMATCH="$4"
 
-
-## Create directories
-mkdir -p "$FILTERED_DIR" "$SORTED_DIR"
-
-
 ## Get basename of input .bam files
 base_name=$(basename "$INPUT_BAM" .bam)
 base_name=${base_name%_mapped}
@@ -26,15 +21,13 @@ base_name=${base_name%_mapped}
 filtered_bam="$FILTERED_DIR/${base_name}_filtered.bam"
 sorted_bam="$SORTED_DIR/${base_name}_sorted.bam"
 
-
 ## Filter reads with less mismatches than max set
 echo "Filtering $INPUT_BAM..."
-if ! samtools view -h "$INPUT_BAM" | grep -E "^@|NM:i:[0-$MAX_MISMATCH]" | samtools view -bS - > "$filtered_bam"; then
+if ! samtools view -h -b -q $MAX_MISMATCH "$INPUT_BAM" > "$filtered_bam"; then
     echo "Error filtering $INPUT_BAM" >&2
     exit 1
 fi
 
-    
 ## Sort filtered bam files
 echo "Sorting $filtered_bam..."
 if ! samtools sort -o "$sorted_bam" "$filtered_bam"; then
@@ -42,13 +35,11 @@ if ! samtools sort -o "$sorted_bam" "$filtered_bam"; then
     exit 1
 fi
 
-
 ## Indexing sorted bam files
 echo "Indexing $sorted_bam..."
 if ! samtools index "$sorted_bam"; then
     echo "Error indexing $sorted_bam" >&2
     exit 1
 fi
-
 
 echo "Processing of $INPUT_BAM done."
