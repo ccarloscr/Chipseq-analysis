2753e6ab8dbc04785c78c9874c26ede36b94118e
ccarloscr <134629785+ccarloscr@users.noreply.github.com>
2025-03-05 18:59:13 +0100

    Update Peak-calling.sh

--- Scripts/Peak-calling.sh
+++ Scripts/Peak-calling.sh
@@ -19,7 +19,7 @@ if [[ ! -f "$METADATA_FILE" ]]; then
 fi
 
 ## Read the first 5 columns of the metadata file
-tail -n +2 "$METADATA_FILE" | while IFS=$',' read -r INPUT_NAME EXP_NAME ANTIBODY CONDITION REP _; do
+tail -n +2 "$METADATA_FILE" | while IFS=';' read -r INPUT_NAME EXP_NAME ANTIBODY CONDITION REP _; do
     INPUT_BAM="${INPUT_DIR}/${INPUT_NAME}_sorted.bam"
     EXP_BAM="${INPUT_DIR}/${EXP_NAME}_sorted.bam"
     OUTPUT_NAME="${ANTIBODY}_${CONDITION}_${REP}"
